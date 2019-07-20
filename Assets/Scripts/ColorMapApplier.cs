6c77be31f25a7ba6215ea42d509adcbbcfaab4b0
crassSandwich <christopherjanidlo@gmail.com>
2019-07-20 19:33:20 -0400

    Color stuff

---
+++ Assets/Scripts/ColorMapApplier.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class ColorMapApplier : MonoBehaviour
+{
+    public MagicColor Color;
+
+    SpriteRenderer render;
+
+    void Start ()
+    {
+        render.color = ColorMap.Value[Color];
+    }
+}
---
+++ Assets/Scripts/Colors.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ColorMap
+{
+    public static Dictionary<MagicColor, Color> Value = new Dictionary<MagicColor, Color>
+    {
+        { MagicColor.Red, Color.red },
+        { MagicColor.Green, Color.green },
+        { MagicColor.Blue, Color.blue }
+    };
+}
+
+public enum MagicColor
+{
+    Red, Green, Blue
+}
