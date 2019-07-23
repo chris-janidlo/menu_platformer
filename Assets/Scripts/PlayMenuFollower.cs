2f612a63c18654dc7d76d944c4da153f1c4a1595
crassSandwich <christopherjanidlo@gmail.com>
2019-07-23 12:37:59 -0400

    Menu follows

---
+++ Assets/Scripts/PlayMenuFollower.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using crass;
+
+public class PlayMenuFollower : MonoBehaviour
+{
+    void Update ()
+    {
+        transform.position = CameraCache.Main.WorldToScreenPoint(MageSquad.Instance.ActiveMage.transform.position);
+    }
+}
