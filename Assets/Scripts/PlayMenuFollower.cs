88f38775e9bcb03ab8b072e995a460534e845745
crassSandwich <christopherjanidlo@gmail.com>
2019-07-23 17:12:21 -0400

    Better following

--- Assets/Scripts/PlayMenuFollower.cs
+++ Assets/Scripts/PlayMenuFollower.cs
@@ -5,8 +5,21 @@ using crass;
 
 public class PlayMenuFollower : MonoBehaviour
 {
+    // 174, 115
+    public Vector2 TopRightLimit;
+
+    Vector3 worldLimit;
+
+    void Start ()
+    {
+        worldLimit = transform.TransformPoint(TopRightLimit);
+    }
+
     void Update ()
     {
-        transform.position = CameraCache.Main.WorldToScreenPoint(MageSquad.Instance.ActiveMage.transform.position);
+        var target = CameraCache.Main.WorldToScreenPoint(MageSquad.Instance.ActiveMage.transform.position);
+        target.x = Mathf.Min(target.x, worldLimit.x);
+        target.y = Mathf.Min(target.y, worldLimit.y);
+        transform.position = target;
     }
 }
