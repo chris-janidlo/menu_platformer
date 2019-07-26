a16e58c5f4fe35422ff621b0153cbc0c3835571e
crassSandwich <christopherjanidlo@gmail.com>
2019-07-26 11:06:52 -0400

    UI in same space as characters, plus new font

--- Assets/Scripts/Menuing/PlayMenuFollower.cs
+++ Assets/Scripts/Menuing/PlayMenuFollower.cs
@@ -6,23 +6,11 @@ using crass;
 public class PlayMenuFollower : MonoBehaviour
 {
     public float FollowTime;
-    public Vector2 TopRightLimit;
 
-    Vector3 worldLimit;
     Vector2 velocity;
-
-    void Start ()
-    {
-        // assuming parent doesn't change
-        worldLimit = transform.TransformPoint(TopRightLimit);
-    }
 
     void Update ()
     {
-        var target = CameraCache.Main.WorldToScreenPoint(MageSquad.Instance.ActiveMage.Visuals.transform.position);
-        target.x = Mathf.Min(target.x, worldLimit.x);
-        target.y = Mathf.Min(target.y, worldLimit.y);
-
-        transform.position = Vector2.SmoothDamp(transform.position, target, ref velocity, FollowTime);
+        transform.position = Vector2.SmoothDamp(transform.position, MageSquad.Instance.ActiveMage.Visuals.transform.position, ref velocity, FollowTime);
     }
 }
