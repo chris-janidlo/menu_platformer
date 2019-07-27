a98edd069ae545aa68532ac812dde8e27c818f0a
crass_sandwich <christopherjanidlo@gmail.com>
2019-07-27 01:25:10 -0400

    Switch to DB16 color palette

--- Assets/Scripts/Colors.cs
+++ Assets/Scripts/Colors.cs
@@ -6,9 +6,9 @@ public static class ColorMap
 {
     public static Dictionary<MagicColor, Color> Value = new Dictionary<MagicColor, Color>
     {
-        { MagicColor.Red, Color.red },
-        { MagicColor.Green, Color.green },
-        { MagicColor.Blue, Color.blue }
+        { MagicColor.Red, new Color(208 / 255f, 70 / 255f, 72 / 255f) },
+        { MagicColor.Green, new Color(109 / 255f, 170 / 255f, 44 / 255f) },
+        { MagicColor.Blue, new Color(89 / 255f, 124 / 255f, 206 / 255f) }
     };
 }
 
