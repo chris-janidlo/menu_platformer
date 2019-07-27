01bd436b91e9c8dd4f61626a13575af33d7b1f5a
crass_sandwich <christopherjanidlo@gmail.com>
2019-07-27 15:18:37 -0400

    Bring back menu clamping

--- Assets/Scripts/Menuing/PlayMenuFollower.cs
+++ Assets/Scripts/Menuing/PlayMenuFollower.cs
@@ -7,10 +7,18 @@ public class PlayMenuFollower : MonoBehaviour
 {
     public float FollowTime;
 
+    public float MinX, MaxX, MaxY;
+
     Vector2 velocity;
 
     void Update ()
     {
-        transform.position = Vector2.SmoothDamp(transform.position, MageSquad.Instance.ActiveMage.Visuals.transform.position, ref velocity, FollowTime);
+        var target = Vector2.SmoothDamp(transform.position, MageSquad.Instance.ActiveMage.Visuals.transform.position, ref velocity, FollowTime);
+
+        transform.position = new Vector2
+        (
+            Mathf.Clamp(target.x, MinX, MaxX),
+            Mathf.Min(target.y, MaxY)
+        );
     }
 }
