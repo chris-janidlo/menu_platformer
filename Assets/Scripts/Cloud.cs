03a34199e11b10cf1bd317eed737a1e94dfc4106
crass_sandwich <christopherjanidlo@gmail.com>
2019-08-10 19:49:22 -0400

    Level design improvements

---
+++ Assets/Scripts/Cloud.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Cloud : MonoBehaviour
+{
+    public int Offset;
+    public float OffsetSwitchTime;
+
+    public SpriteRenderer Visuals;
+
+    IEnumerator Start ()
+    {
+        while (true)
+        {
+            Visuals.transform.localPosition = Vector2.right * Offset;
+            Offset *= -1;
+
+            yield return new WaitForSeconds(OffsetSwitchTime);
+        }
+    }
+}
